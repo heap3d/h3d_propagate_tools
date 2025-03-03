0e429c94f12f8a31d6bc2d70b4e6289074ee8a60
Dmytro Holub <heap3d@gmail.com>
2025-03-03 09:40:59 +0200

    removed" set scale refactoring

--- scripts/replace_with_instance.py
+++ scripts/replace_with_instance.py
@@ -7,7 +7,7 @@
 # Replace selected items with the instance of last selected
 # ================================
 
-from typing import Union
+from typing import Optional
 
 import modo
 import modo.constants as c
@@ -65,6 +65,8 @@ def replace_with_instance(source_item: modo.Item, target_item: modo.Item) -> mod
     fn_in()
     prints(source_item)
     prints(target_item)
+    prints(get_item_scale(source_item))
+    prints(get_item_scale(target_item))
     if not source_item:
         raise ValueError('Source item error: value is None')
     if not target_item:
@@ -79,8 +81,8 @@ def replace_with_instance(source_item: modo.Item, target_item: modo.Item) -> mod
     instance_item.name = instance_name
     instance_item.setParent()
     match_pos_rot(instance_item, target_item)
-    match_scl(instance_item, get_instance_source(source_item))
-    set_scale_factor(instance_item, get_ratios(source_item, target_item))
+    match_scl(instance_item, target_item)
+    prints(get_ratios(source_item, target_item))
 
     replace_item(remove_item=target_item, insert_item=instance_item)
 
@@ -104,7 +106,7 @@ def get_parent_index(item: modo.Item) -> int:
     return 0
 
 
-def parent_items_to(items: list[modo.Item], parent: Union[None, modo.Item], index=0, inplace=True):
+def parent_items_to(items: list[modo.Item], parent: Optional[modo.Item], index=0, inplace=True):
     inplace_num = 1 if inplace else 0
     for item in items:
         if not parent:
@@ -122,47 +124,59 @@ def match_scl(item: modo.Item, itemTo: modo.Item):
     lx.eval(f'item.match item scl average:false item:{{{item.id}}} itemTo:{{{itemTo.id}}}')
 
 
-def get_ratios(source: modo.Item, target: modo.Item) -> list[float]:
+def get_ratios(source: modo.Item, target: modo.Item) -> modo.Vector3:
     source_size = get_source_size(source)
     target_size = get_size(target)
 
     if any([f == 0.0 for f in (source_size + target_size)]):
-        ratio_x = 1.0
-        ratio_y = 1.0
-        ratio_z = 1.0
+        ratio = modo.Vector3(1, 1, 1)
     else:
-        ratio_x = target_size[0] / source_size[0]
-        ratio_y = target_size[1] / source_size[1]
-        ratio_z = target_size[2] / source_size[2]
+        ratio = modo.Vector3(
+            target_size.x / source_size.x,
+            target_size.y / source_size.y,
+            target_size.z / source_size.z
+        )
 
-    return [ratio_x, ratio_y, ratio_z]
-
-
-def get_source_size(source: modo.Item) -> list[float]:
-    source_scl_x, source_scl_y, source_scl_z = get_item_scale(source)
-    base_scl_x, base_scl_y, base_scl_z = get_size(get_instance_source(source))
-    source_size = [base_scl_x * source_scl_x, base_scl_y * source_scl_y, base_scl_z * source_scl_z]
-
-    return source_size
+    return ratio
 
 
-def get_size(item: modo.Item) -> list[float]:
+def get_source_size(source: modo.Item) -> modo.Vector3:
+    source_scl = get_item_scale(source)
+    base_scl = get_size(get_instance_source(source))
+
+    return modo.Vector3(
+        base_scl.x * source_scl.x,
+        base_scl.y * source_scl.y,
+        base_scl.z * source_scl.z
+    )
+
+
+def get_size(item: modo.Item) -> modo.Vector3:
+    prints(item)
     if item.type != 'mesh' and item.type != 'meshInst':
-        return [0.0, 0.0, 0.0]
+        return modo.Vector3(1, 1, 1)
 
-    s_x, s_y, s_z = get_item_scale(item)
+    scl = get_item_scale(item)
     if item.type == 'meshInst':
         corners = get_instance_source(item).geometry.boundingBox
     else:
         corners = item.geometry.boundingBox
-    size_x = abs(corners[1][0] - corners[0][0]) * s_x
-    size_y = abs(corners[1][1] - corners[0][1]) * s_y
-    size_z = abs(corners[1][2] - corners[0][2]) * s_z
+    size = modo.Vector3()
+    size.x = abs(corners[1][0] - corners[0][0]) * scl.x
+    if size.x == 0:
+        size.x = 1
+    size.y = abs(corners[1][1] - corners[0][1]) * scl.y
+    if size.y == 0:
+        size.y = 1
+    size.z = abs(corners[1][2] - corners[0][2]) * scl.z
+    if size.z == 0:
+        size.z = 1
 
-    return [size_x, size_y, size_z]
+    return size
 
 
-def get_item_scale(item: modo.Item) -> list[float]:
+def get_item_scale(item: modo.Item) -> modo.Vector3:
+    prints(item)
     item.select(replace=True)
     x = lx.eval(f'transform.channel scl.X ? item:{{{item.id}}}')
     if x is None:
@@ -174,18 +188,20 @@ def get_item_scale(item: modo.Item) -> list[float]:
     if z is None:
         z = 1.0
 
-    return [x, y, z]
+    prints(modo.Vector3(x, y, z))
+
+    return modo.Vector3(x, y, z)
 
 
-def set_scale_factor(item: modo.Item, factor: list[float]):
-    s_x, s_y, s_z = get_item_scale(item)
-    set_item_scale(item, [s_x * factor[0], s_y * factor[1], s_z * factor[2]])
+def set_scale_factor(item: modo.Item, factor: modo.Vector3):
+    scl = get_item_scale(item)
+    set_item_scale(item, modo.Vector3(scl.x * factor.x, scl.y * factor.y, scl.z * factor.z))
 
 
-def set_item_scale(item: modo.Item, scale: list[float]):
-    lx.eval(f'transform.channel scl.X {scale[0]} item:{{{item.id}}}')
-    lx.eval(f'transform.channel scl.Y {scale[1]} item:{{{item.id}}}')
-    lx.eval(f'transform.channel scl.Z {scale[2]} item:{{{item.id}}}')
+def set_item_scale(item: modo.Item, scale: modo.Vector3):
+    lx.eval(f'transform.channel scl.X {scale.x} item:{{{item.id}}}')
+    lx.eval(f'transform.channel scl.Y {scale.y} item:{{{item.id}}}')
+    lx.eval(f'transform.channel scl.Z {scale.z} item:{{{item.id}}}')
 
 
 if __name__ == '__main__':
